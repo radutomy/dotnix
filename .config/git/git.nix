dff539e61a7209b377ec90b975529f5a226df656
Radu T <radu@rtom.dev>
2026-02-03 18:40:08 +0000

    git.nix

--- .config/git/git.nix
+++ .config/git/git.nix
@@ -1,24 +1,27 @@
 { pkgs, ... }:
 {
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      line-numbers = true;
+    };
+  };
+
   programs.git = {
     enable = true;
-    userName = "Radu T";
-    userEmail = "radu@rtom.dev";
 
     signing = {
       key = "/root/.ssh/id_ed25519";
       signByDefault = true;
     };
 
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        line-numbers = true;
+    settings = {
+      user = {
+        name = "Radu T";
+        email = "radu@rtom.dev";
       };
-    };
-
-    extraConfig = {
       init.defaultBranch = "main";
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
