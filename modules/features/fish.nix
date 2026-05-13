5a828dae2991a1d5807c5133909416b7fd046161
radutomy <radu@rtom.dev>
2026-05-13 19:31:15 +0000

    tmux

--- modules/features/fish.nix
+++ modules/features/fish.nix
@@ -24,7 +24,7 @@ _: {
         fish = {
           enable = true;
           shellInit = ''
-            function fish_title; echo (prompt_pwd)  $HOST_ICON (hostname); end
+            function fish_title; echo (prompt_pwd)    $HOST_ICON (hostname); end
 
             set fish_prompt_pwd_dir_length 100 # max length of dir path
             set fish_greeting # surpress fish greeting
