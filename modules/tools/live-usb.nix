2f1c5da09a6b87723f9ee3bb6e378c5e21d6944a
radutomy <radu@rtom.dev>
2026-05-15 17:19:41 +0000

    live-usb

---
+++ modules/tools/live-usb.nix
@@ -0,0 +1,64 @@
+{ self, inputs, ... }:
+{
+  flake.nixosConfigurations.liveUsb = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
+      "${inputs.nixpkgs}/nixos/modules/profiles/minimal.nix"
+
+      (
+        { config, lib, ... }:
+        {
+          isoImage = {
+            makeEfiBootable = true;
+            makeUsbBootable = true;
+          };
+
+          fileSystems = lib.mkImageMediaOverride config.lib.isoFileSystems;
+          swapDevices = lib.mkImageMediaOverride [ ];
+
+          networking.hostName = "live-usb";
+          networking.useDHCP = true;
+
+          boot.loader.timeout = lib.mkForce 1;
+          boot.zfs.forceImportRoot = false;
+
+          services.openssh = {
+            enable = true;
+            openFirewall = true;
+            settings = {
+              PasswordAuthentication = false;
+              KbdInteractiveAuthentication = false;
+              PermitRootLogin = "prohibit-password";
+            };
+          };
+
+          users.users.root.openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOcSG9I0xIYG43LhgnsfR7Y1hOkoVpE5RGSfgr3usDt9 radu@rtom.dev"
+          ];
+
+          system.stateVersion = "25.11";
+        }
+      )
+    ];
+  };
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      apps.liveUsb.program =
+        let
+          inherit (self.nixosConfigurations.liveUsb.config.system.build) isoImage;
+        in
+        "${
+          pkgs.writeShellApplication {
+            name = "copy-live-usb-iso";
+            runtimeInputs = [ pkgs.coreutils ];
+            text = ''
+              cp ${isoImage}/iso/*.iso /tmp/live-usb.iso
+              echo /tmp/live-usb.iso
+            '';
+          }
+        }/bin/copy-live-usb-iso";
+    };
+}
