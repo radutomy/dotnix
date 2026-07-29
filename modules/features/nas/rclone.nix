67760c9a9b6bc32c78e887fc976a1619ea3eb237
Radu T <radu@rtom.dev>
2026-07-29 08:55:56 +0100

    gdrive

--- modules/features/nas/rclone.nix
+++ modules/features/nas/rclone.nix
@@ -4,32 +4,71 @@ _: {
     {
       age.secrets."rclone.conf".file = ../../../secrets/rclone.age;
 
-      systemd.services.gdrive-sync = {
-        description = "Mirror Google Drive to /drive";
-        startAt = "03:00";
-        wants = [ "network-online.target" ];
-        after = [ "network-online.target" ];
+      programs.fuse.userAllowOther = true;
+      systemd = {
 
-        environment = {
-          RCLONE_CONFIG = config.age.secrets."rclone.conf".path;
-          RCLONE_CREATE_EMPTY_SRC_DIRS = "true";
-          RCLONE_DRIVE_ACKNOWLEDGE_ABUSE = "true";
+        tmpfiles.settings.gdrive = {
+          "/gdrive".d = {
+            user = "root";
+            group = "root";
+            mode = "0755";
+          };
+          "/var/cache/rclone-gdrive".d = {
+            user = "root";
+            group = "root";
+            mode = "0700";
+          };
         };
 
-        unitConfig.RequiresMountsFor = "/drive";
+        services.gdrive = {
+          description = "Live Google Drive mount at /gdrive";
+          wantedBy = [ "multi-user.target" ];
+          wants = [ "network-online.target" ];
+          after = [ "network-online.target" ];
 
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.rclone}/bin/rclone sync gdrive: /drive";
+          environment.RCLONE_CONFIG = config.age.secrets."rclone.conf".path;
 
-          CapabilityBoundingSet = "";
-          NoNewPrivileges = true;
-          PrivateDevices = true;
-          PrivateTmp = true;
-          ProtectHome = true;
+          serviceConfig = {
+            ExecStart = ''
+              ${pkgs.rclone}/bin/rclone mount gdrive: /gdrive \
+                --allow-other \
+                --cache-dir /var/cache/rclone-gdrive \
+                --poll-interval 15s \
+                --vfs-cache-mode writes \
+                --vfs-cache-max-size 20G
+            '';
+            Restart = "on-failure";
+            RestartSec = "10s";
+          };
         };
+
+        services.gdrive-mirror = {
+          description = "Mirror Google Drive to /drive";
+          startAt = "03:00";
+          wants = [ "network-online.target" ];
+          after = [ "network-online.target" ];
+
+          environment = {
+            RCLONE_CONFIG = config.age.secrets."rclone.conf".path;
+            RCLONE_CREATE_EMPTY_SRC_DIRS = "true";
+            RCLONE_DRIVE_ACKNOWLEDGE_ABUSE = "true";
+          };
+
+          unitConfig.RequiresMountsFor = "/drive";
+
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = "${pkgs.rclone}/bin/rclone sync gdrive: /drive";
+
+            CapabilityBoundingSet = "";
+            NoNewPrivileges = true;
+            PrivateDevices = true;
+            PrivateTmp = true;
+            ProtectHome = true;
+          };
+        };
+
+        timers.gdrive-mirror.timerConfig.Persistent = true;
       };
-
-      systemd.timers.gdrive-sync.timerConfig.Persistent = true;
     };
 }
