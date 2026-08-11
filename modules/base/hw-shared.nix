8213813290d23051f4aea4121a895179fd81edca
Radu T <radu@rtom.dev>
2026-08-11 16:19:39 +0100

    appimage

--- modules/base/hw-shared.nix
+++ modules/base/hw-shared.nix
@@ -2,6 +2,9 @@ _: {
   flake.modules.nixos.hwshared = {
     services.fwupd.enable = true;
 
+    programs.appimage.enable = true;
+    programs.appimage.binfmt = true;
+
     # Caps Lock is disabled and repurposed as a nav layer instead:
     # caps+left/right/up/down = home/end/page up/down.
     # caps+h/j/k/l = left/down/up/right arrows.
