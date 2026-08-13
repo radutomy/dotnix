2989d20709d63353ddd537e04b9c1362caa1f2ff
Radu T <radu@rtom.dev>
2026-08-13 10:20:54 +0100

    keyboard

--- modules/base/hw-shared.nix
+++ modules/base/hw-shared.nix
@@ -8,24 +8,42 @@ _: {
     # Caps Lock is disabled and repurposed as a nav layer instead:
     # caps+left/right/up/down = home/end/page up/down.
     # caps+h/j/k/l = left/down/up/right arrows.
-    services.keyd = {
+    # caps+h twice quickly = Home, caps+l twice quickly = End
+    # (kanata's tap-dance; keyd doesn't support double-tap detection,
+    # hence kanata over keyd).
+    services.kanata = {
       enable = true;
-      keyboards.default = {
-        ids = [ "*" ];
-        settings = {
-          main.capslock = "layer(nav)";
-          nav = {
-            left = "home";
-            right = "end";
-            up = "pageup";
-            down = "pagedown";
-            h = "left";
-            j = "down";
-            k = "up";
-            l = "right";
-          };
-        };
-      };
+      keyboards.default.config = ''
+        (defsrc
+          caps
+          left right up down
+          h j k l
+        )
+
+        (deflayermap (base)
+          caps (layer-while-held nav)
+          left left
+          right right
+          up up
+          down down
+          h h
+          j j
+          k k
+          l l
+        )
+
+        (deflayermap (nav)
+          caps _
+          left home
+          right end
+          up pgup
+          down pgdn
+          h (tap-dance-eager 180 (left home))
+          j down
+          k up
+          l (tap-dance-eager 180 (right end))
+        )
+      '';
     };
   };
 }
